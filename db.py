13e7d4b5ef51c4f3cd92fd6cbc26304ecab8d6ce
Murat Ayfer <murat@ayfer.net>
2013-11-22 15:49:11 -0800

    some stuffs

---
+++ db.py
@@ -0,0 +1,10 @@
+
+from redis import Redis
+
+
+class History(object):
+    def __init__(self):
+        self.redis = Redis(host='localhost', port=6379, db=0, password=None, socket_timeout=None, connection_pool=None, charset='utf-8', errors='strict', decode_responses=False, unix_socket_path=None)
+
+    def append(self, spiel):
+        
--- game.py
+++ game.py
@@ -15,39 +15,32 @@ class GameState(object):
         return player
 
     def remove_player(self, session_id):
-        if self.hat.owner == session_id:
-            self.hat.owner = None
-            self.hat.position.update(self.players[session_id].position.x, self.players[session_id].position.y)
         del self.players[session_id]
 
     def post_spiel(self, spiel):
-        self.spiels.append(spiel)
+        self.history.append(spiel)
 
-    def dictify(self):
-        players = {}
-        hat = self.hat.dictify()
-        for key, val in self.players.items():
-            players[key] = val.dictify()
-        return {'players': players, 'hat': hat}
 
 class Player(object):
-    # Guy types should be updated if new character models added
-    guy_types = [1, 2, 3]
 
     def __init__(self, session_id):
         self.id = session_id
-        # Player initial spawn position
-        self.position = Position(0, 0)
-        self.movement = Movement(0, 0)
-        self.character = choice(self.guy_types)
-        self.speech = None;
+        self.color = self.generate_color()
+
+    def generate_color():
+        return "#ffffff"
 
     def set_location(self, latitude, longitude):
         self.latitude = latitude
         self.longitude = longitude
 
     def dictify(self):
-        return {'id': self.id, 'position': {'x': self.position.x, 'y': self.position.y}, 'character': self.character, 'movement': {'dx': self.movement.dx, 'dy': self.movement.dy}, 'speech': self.speech}
+        return {
+            'id': self.id,
+            'color': self.color,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+        }
 
 class Spiel(object):
     def __init__(self):
@@ -56,25 +49,12 @@ class Spiel(object):
         self.owner = None
 
     def dictify(self):
-        return { 'owner': self.owner, 'latitude': self.latitude, 'y': self.longitude }
+        return {
+            'owner': self.owner,
+            'latitude': self.latitude,
+            'y': self.longitude
+        }
 
     def json(self):
         return json_encode(self.dictify())
 
-class Position(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def update(self, x, y):
-        self.x = x
-        self.y = y
-
-class Movement(object):
-    def __init__(self, dx, dy):
-        self.dx = dx
-        self.dy = dy
-
-    def update(self, dx, dy):
-        self.dx = dx
-        self.dy = dy
