c428fd90a4fc58c0915372c83a771b53c34c8893
Murat Ayfer <murat@ayfer.net>
2013-11-27 01:59:38 -0800

    full rectangle

--- geo.py
+++ geo.py
@@ -8,5 +8,6 @@ class Geo(object):
             ('%.2f' % (latitude + 0.01), '%.2f' % longitude),
             ('%.2f' % (latitude + 0.01), '%.2f' % (longitude + 0.01)),
             ('%.2f' % latitude, '%.2f' % (longitude + 0.01)),
+            ('%.2f' % latitude, '%.2f' % longitude),
         ]
         return '|'.join([ ','.join(coords) for coords in poly ])
