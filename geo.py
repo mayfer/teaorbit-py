aa0adbfad398a8f85945b2fc950d1d3304ed5f62
Murat Ayfer <murat@ayfer.net>
2013-12-05 01:26:10 -0800

    geo fix

--- geo.py
+++ geo.py
@@ -1,13 +1,23 @@
 
+
+
 class Geo(object):
+    def _truncate(self, f, n):
+        '''Truncates/pads a float f to n decimal places without rounding'''
+        return float(('%.*f' % (n + 1, f))[:-1])
+
     def get_block_id(self, latitude, longitude):
         latitude = float(latitude)
         longitude = float(longitude)
+
+        flat_lat = self._truncate(latitude, 2)
+        flat_long = self._truncate(longitude, 2)
+
         poly = [
-            ('%.2f' % (latitude + 0.08), '%.2f' % (longitude - 0.06)),
-            ('%.2f' % (latitude - 0.04), '%.2f' % (longitude - 0.06)),
-            ('%.2f' % (latitude - 0.04), '%.2f' % (longitude + 0.06)),
-            ('%.2f' % (latitude + 0.08), '%.2f' % (longitude + 0.06)),
-            ('%.2f' % (latitude + 0.08), '%.2f' % (longitude - 0.06)),
+            ('%.2f' % (flat_lat + 0.00), '%.2f' % (flat_long - 0.00)),
+            ('%.2f' % (flat_lat + 0.01), '%.2f' % (flat_long - 0.00)),
+            ('%.2f' % (flat_lat + 0.01), '%.2f' % (flat_long - 0.01)),
+            ('%.2f' % (flat_lat + 0.00), '%.2f' % (flat_long - 0.01)),
+            ('%.2f' % (flat_lat + 0.00), '%.2f' % (flat_long - 0.00)),
         ]
         return '|'.join([ ','.join(coords) for coords in poly ])
