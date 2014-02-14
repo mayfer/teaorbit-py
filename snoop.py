2bd6f848ebdb1b10fd4dd6d96a4ff0451bb91cc7
Murat Ayfer <murat@ayfer.net>
2014-02-14 02:33:57 +0000

    snoop shows recent activity properly

--- snoop.py
+++ snoop.py
@@ -1,7 +1,16 @@
 from db import History
+import simplejson as json
+
 h = History()
 keys = [ key[6:] for key in h.redis.keys('block:*') ]
+things = []
 for key in keys:
     spiels = h.get_spiels(key)
     last = spiels[-1]
-    print key, last
+    spiel = json.loads(last)
+    things.append((key, last, spiel['date']))
+
+things = sorted(things, key=lambda x: x[2])
+
+for thing in things[-20:]:
+    print thing[0], thing[1]
