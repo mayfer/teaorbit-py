27a4cfa5bdab977854c08e78ab82941ce696d537
Murat Ayfer <murat@ayfer.net>
2014-10-25 20:19:33 -0700

    selecting text doesnt deselect now

--- static/scripts/basics.js
+++ static/scripts/basics.js
@@ -57,3 +57,10 @@ function uuid(){
     });
     return uuid;
 };
+
+function pixel_distance(x1, y1, x2, y2) {
+    return Math.sqrt(
+        Math.pow(x1 - x2, 2),
+        Math.pow(y1 - y2, 2)
+    )
+}
